2381821a21313e16c0e2fcf546ea4a498612cc6f
Bobybyk <matt.lefranc@gmail.com>
2023-12-04 23:23:03 +0100

    Refactor send_question function

--- Main.py
+++ Main.py
@@ -6,43 +6,39 @@ import re
 import requests
 
 def send_question(question, reponse, image):
-    img_path = 'img/' + image
-    if image != "NULL":
-        file = {'file': ('image', open(img_path, 'rb'), 'image/png')}
-    else:
-        file = "null"
-    requests.post("http://localhost:3000/api/cours/architecture/questionEvent", {"question": question, "reponse": reponse, "image": file})
+    img_path = 'img/' + image if image != "NULL" else None
+
+    files = None
+    if img_path:
+        return
+        files = {'image': open(img_path, 'rb')}
+    data = {"question": question, "reponse": reponse}
+
+    requests.post("http://87.106.120.243:3000/api/cours/architecture/questionEvent", data=data)
 
 def question_manager():
     while True:
         random_question = random.choice(DB.DataBase.BASE)
-        # Afficher la question
+
         print("Question : " + random_question.question + "\nRéponse : " + random_question.reponse + "\nImage : " + random_question.imgQuestion)
-        print("=" * 30)  # Ajoute une ligne de séparation
+        print("=" * 30)
 
         send_question(random_question.question, random_question.reponse, random_question.imgQuestion)
 
-        # Attente avant de tirer la question suivante
-        time.sleep(60 * 60 * 2)  # Attendre 2 heures entre chaque question
+        time.sleep(60)
 
 def display_database_content():
     for question in DB.DataBase.BASE:
         print(question.toString())
-        print("=" * 30)  # Ajoute une ligne de séparation entre chaque question
+        print("=" * 30)
 
 def main():
-    # ----- Init database
+
     parser = DB.Parser()
     print("Nombre de questions dans la base de données : " + str(len(DB.DataBase.BASE)))
 
-    # ----- Afficher le contenu de la base de données
-    #display_database_content()
-
-    # ----- Tirer une question aléatoire toutes les 2 heures
-    # ----- Créer un thread pour exécuter daily_question
     question_thread = threading.Thread(target=question_manager)
 
-    # ----- Démarrer le thread
     question_thread.start()
 
 if __name__ == "__main__":
