c0daa2437e3407efbd9b5f856b515b60160d5365
Bobybyk <matt.lefranc@gmail.com>
2023-12-10 12:34:25 +0100

    Update data and question manager in Main.py

--- Main.py
+++ Main.py
@@ -2,39 +2,36 @@ import DB
 import random
 import threading
 import time
-import re
 import requests
 
-def send_question(question, reponse, image):
-    img_path = 'img/' + image if image != "NULL" else None
-
-    files = None
-    if img_path:
-        return
-        files = {'image': open(img_path, 'rb')}
+def send_question(question, reponse):
     data = {"question": question, "reponse": reponse}
-
     requests.post("http://localhost:3000/api/cours/architecture/questionEvent", data=data)
+
 
 def question_manager():
     while True:
-        random_question = random.choice(DB.DataBase.BASE)
 
-        print("Question : " + random_question.question + "\nRéponse : " + random_question.reponse + "\nImage : " + random_question.imgQuestion)
+        random_question = random.choice(DB.DataBase.BASE)
+        
+        while (random_question.imgQuestion == "NULL"):
+            random_question = random.choice(DB.DataBase.BASE)
+
+        print("Question : " + random_question.question + "\nRéponse : " + random_question.reponse)
         print("=" * 30)
 
-        send_question(random_question.question, random_question.reponse, random_question.imgQuestion)
-        time.sleep(60 * 60)
+        send_question(random_question.question, random_question.reponse)
+
+        time.sleep(60 * 30)
+
 
 def display_database_content():
     for question in DB.DataBase.BASE:
         print(question.toString())
         print("=" * 30)
 
-def main():
 
-    parser = DB.Parser()
-    print("Nombre de questions dans la base de données : " + str(len(DB.DataBase.BASE)))
+def main():
 
     question_thread = threading.Thread(target=question_manager)
 
